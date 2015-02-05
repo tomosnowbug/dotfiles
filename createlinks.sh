5fa68b39c93f10a8b0555799b317a9355f12f016
zztsasaki3 <tomokazu.sasaki@uniadex.co.jp>
2015-02-05 21:00:42 +0900

    clone先ディレクトリが~/git/dotfilesでなくても動作するように

--- createlinks.sh
+++ createlinks.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-ln -sf ~/git/dotfiles/.vimrc ~/.vimrc
-mkdir ~/git/dotfiles/.vim
-ln -sf ~/git/dotfiles/.vim ~/.vim
+PWD=`pwd`
+
+ln -sf ${PWD}/vimrc ~/.vimrc
+mkdir ~/.vim
 #ln -sf ~/git/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/git/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ${PWD}/.tmux.conf ~/.tmux.conf
 
 
