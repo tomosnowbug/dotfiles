a5087ced15902e62ddb99aefabcf9da9c9380d34
tomosnowbug <tomo.snowbug@gmail.com>
2023-11-02 11:51:28 +0900

    dotfilesのオリジナルを保存するように改変

--- createlinks.sh
+++ createlinks.sh
@@ -2,13 +2,18 @@
 
 PWD=`pwd`
 
+mv ~/.vimrc ~/.vimrc_orig
 ln -sf ${PWD}/.vimrc ~/.vimrc
-ln -sf ${PWD}/.zshrc ~/.zshrc
 mkdir ~/.vim
+
+mv ~/.zshrc ~/.zshrc_orig
+ln -sf ${PWD}/.zshrc ~/.zshrc
+
+mv ~/.tmux.conf ~/.tmux.conf_orig
 ln -sf ${PWD}/.tmux.conf ~/.tmux.conf
 
+mv ~/.bashrc ~/.bashrc_orig
 ln -sf ${PWD}/.bashrc ~/.bashrc
 ln -sf ${PWD}/.bashrc_forcygwin ~/.bashrc_forcygwin
 ln -sf ${PWD}/.bash_aliases ~/.bash_aliases
 #ln -sf ${PWD}/.
-
